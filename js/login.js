8e75a25eae6b4853f978fbc233f18f38efddca4f
Nicolas Lois <nicolaslois90@gmail.com>
2023-09-01 20:05:29 -0300

    la pelota al 10

--- js/login.js
+++ js/login.js
@@ -3,7 +3,7 @@ async function iniciarSesion() {
   const password = document.getElementById("contraseña").value;
 
   const request = await fetch(
-    "https://52.226.225.249:8080/login?username=" +
+    "http://4.157.191.218:8080/login?username=" +
       username +
       "&password=" +
       password,
